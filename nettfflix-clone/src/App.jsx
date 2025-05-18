98e4013a2a039d5c0925f74e76c653bc7b1e9e6c
omer-siraj <omerdinsiraj@gmail.com>
2025-05-18 10:43:17 -0700

    header and footer

--- nettfflix-clone/src/App.jsx
+++ nettfflix-clone/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-
 import './App.css'
+import Home from './pages/Home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <div className='App'>
-           <h1>test</h1>
+           <Home/>
     </div>
     
     </>
---
+++ nettfflix-clone/src/pages/Home/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+
+const Home = () => {
+  return (
+    <>
+        <Header />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <Footer />
+
+
+    </>
+  )
+}
+
+export default Home
